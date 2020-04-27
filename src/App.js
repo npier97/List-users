a141250b5577f3d2323b181ab71d0ca98386b1d6
T0862 <t0862@T0862s-MacBook-Pro.local>
2020-04-27 19:53:54 +0300

    display the list of users

--- src/App.js
+++ src/App.js
@@ -3,23 +3,40 @@ import React, { useEffect, useState } from "react";
 const App = () => {
   const [usersList, setUsersList] = useState({});
 
-  async function fetchData() {
+  const fetchData = () => {
     const api = "https://reqres.in/api/users?page=2";
 
-    await fetch(api)
+    fetch(api)
       .then((response) => response.json())
       .then((response) => setUsersList({ usersList: response }));
-  }
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return (
-    <div>
-      <ul id="list"></ul>
-    </div>
-  );
+  const displayUsers = () => {
+    const list = document.getElementById("parentElement");
+
+    for (const key in usersList) {
+      const values = usersList[key];
+      const users = values.data;
+
+      users.forEach((element) => {
+        const node = document.createElement("ul");
+        let string = "";
+
+        for (const key in element) {
+          const values = element[key];
+          string += `<li>${values}</li>`;
+          node.innerHTML = string;
+        }
+        list.appendChild(node);
+      });
+    }
+  };
+
+  return <div id="parentElement">{displayUsers()}</div>;
 };
 
 export default App;
